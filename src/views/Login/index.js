ca4d25ac5e402a0a42991ac3d3616a9dcefee7ee
Muhammad Al-Pandi <mh2.alpandi@yahoo.co.id>
2019-10-28 17:13:08 +0700

    Login Form

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+  const [ values, setValues ] = useState({
+    username: '',
+    password: ''
+  });
+
+  const [ touched, setTouched ] = useState({
+    username: false,
+    password: false
+  });
+
+  const handleChange = e => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleBlur = field => evt => {
+    this.setTouched({
+      ...touched,
+      [field]: true
+    });
+  };
+
   return (
-    <div>
-      Login Page
-    </div>
+    <form>
+      <label>
+        Username:
+        <input
+          value={values.username}
+          name="username"
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          value={values.password}
+          name="password"
+          type="password"
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <button>Submit</button>
+    </form>
   );
 }
 
