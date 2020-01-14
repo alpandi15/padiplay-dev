67f527644f5308997bdf20c95de348622773b183
Muhammad Al-Pandi <mh2.alpandi@yahoo.co.id>
2020-01-14 10:13:19 +0700

    Menu active

--- src/layouts/partials/HeaderMenu.js
+++ src/layouts/partials/HeaderMenu.js
@@ -8,39 +8,46 @@ import IconShop from 'assets/img/icon/icon-shop.svg'
 import IconCaster from 'assets/img/icon/icon-casterr.svg'
 
 const Menu = () => {
+  const checkActive = (match, location) => {
+    if (!location) return false
+    const { pathname } = location
+    console.log(pathname)
+    return pathname === '/'
+  }
+
   return (
     <>
-      <li className="nav-item d-block d-md-none">
+      <NavLink activeClassName="active" isActive={checkActive} className="nav-item d-block d-md-none" to="/">
         <div className="text-center mb-1">
           <img src={IconHome} className="icon-app icon-invert" alt="" />
         </div>
-        <NavLink activeClassName="active" className="nav-link link-app" to="/">Home</NavLink>
-      </li>
-      <li className="nav-item">
+        <div className="nav-link link-app">Home</div>
+      </NavLink>
+      <NavLink className="nav-item" activeClassName="active" to="/games">
         <div className="text-center d-block d-md-none mb-1">
           <img src={IconGames} className="icon-app icon-invert" alt="" />
         </div>
-        <NavLink activeClassName="active" className="nav-link link-app" to="/games">Games</NavLink>
-      </li>
-      <li className="nav-item">
+        <div className="nav-link link-app">Games</div>
+      </NavLink>
+      <NavLink className="nav-item" activeClassName="active" to="/organizer">
         <div className="text-center d-block d-md-none mb-1">
           <img src={IconCommunity} className="icon-app icon-invert" alt="" />
         </div>
-        <NavLink className="nav-link link-app" to="/organizer">Community</NavLink>
-      </li>
-      <li className="nav-item">
+        <div className="nav-link link-app">Community</div>
+      </NavLink>
+      <NavLink className="nav-item" activeClassName="active" to="/shop">
         <div className="text-center d-block d-md-none mb-1">
           <img src={IconShop} className="icon-app icon-invert" alt="" />
         </div>
-        <NavLink className="nav-link link-app" to="/shop">Shop</NavLink>
-      </li>
-      <li className="nav-item d-block d-md-none">
+        <div className="nav-link link-app">Shop</div>
+      </NavLink>
+      <NavLink className="nav-item d-block d-md-none" to="/caster" activeClassName="active">
         <div className="text-center d-block d-md-none mb-1">
           <img src={IconCaster} className="icon-app icon-invert" alt="" />
         </div>
-        <NavLink className="nav-link link-app" to="/caster">Caster</NavLink>
-      </li>
-      <li className="nav-item dropdown mr-3 d-none d-md-block">
+        <div className="nav-link link-app">Caster</div>
+      </NavLink>
+      <div className="nav-item dropdown mr-3 d-none d-md-block">
         <div style={{ cursor: 'pointer' }} className="nav-link link-app dropdown-toggle more" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         More
         </div>
@@ -49,7 +56,7 @@ const Menu = () => {
           <NavLink className="dropdown-item" to="/blog">Blog</NavLink>
           <NavLink className="dropdown-item" to="/about">About</NavLink>
         </div>
-      </li>
+      </div>
     </>
   )
 }
